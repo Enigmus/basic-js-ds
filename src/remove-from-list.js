95304d51ff9027149ad19a4fb9b4f6d8ac6d4066
sd <sd_net@mail.ru>
2023-12-23 22:44:09 +0300

    feat: add full solution

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -29,39 +29,21 @@ function removeKFromList(l, k) {
     while(current){
       if(current.value !== k){
         prev = current;
-        current = current.next
+        current = current.next;
       }
       else{
-        prev.next = current.next
-        current = l.next
+        if(prev){
+          prev.next = current.next;
+          current = l.next;
+        }else{  
+          current.value = l.next.value
+          current.next = l.next.next;          
+        } 
       }
     }
     
     return l;
 }
-
-class ListNode {
-    constructor(x) {
-        this.value = x;
-        this.next = null;
-    }
-}
-
-function convertArrayToList(arr) {
-    return arr.reverse().reduce((acc, cur) => {
-        if (acc) {
-            const node = new ListNode(cur);
-            node.next = acc;
-            return node;
-        }
-
-        return new ListNode(cur);
-    }, null);
-}
-
-const initial = convertArrayToList([3, 1, 2, 3, 4, 5]);
-
-console.log((removeKFromList(initial, 1)));
 
 module.exports = {
     removeKFromList,
